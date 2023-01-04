06ec7d9aff0a5b081975d47cdcc73aec06ee449a
Eric Hogberg <misc@erichogberg.com>
2023-01-03 21:34:01 -0600

    Add os monitor support.

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Weather.MixProject do
   def application do
     [
       mod: {Weather.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
